ef41726accc303b94f986d5d7efd2e2f5be46588
jas4code <jasabocejo@protonmail.com>
2023-01-15 02:16:30 +0800

    Removed redundancy from the JS file

--- app.js
+++ app.js
@@ -1,87 +1,9 @@
-$(document).ready(function() {
-    $('.candy').show()
-   
-    $('#candy-1').click(function() {
-        $('#candy-1').hide();
-    })
-
-    $('#candy-2').click(function() {
-        $('#candy-2').hide();
-    })
-
-    $('#candy-3').click(function() {
-        $('#candy-3').hide();
-    })
-
-    $('#candy-4').click(function() {
-        $('#candy-4').hide();
-    })
-    
-    $('#candy-5').click(function() {
-        $('#candy-5').hide();
-    })
-    
-    $('#candy-6').click(function() {
-        $('#candy-6').hide();
-    })
-    
-    $('#candy-7').click(function() {
-        $('#candy-7').hide();
-    })
-    
-    $('#candy-8').click(function() {
-        $('#candy-8').hide();
-    })
-    
-    $('#candy-9').click(function() {
-        $('#candy-9').hide();
-    })
-    
-    $('#candy-10').click(function() {
-        $('#candy-10').hide();
-    })
-    
-    $('#candy-11').click(function() {
-        $('#candy-11').hide();
-    })
-    
-    $('#candy-12').click(function() {
-        $('#candy-12').hide();
-    })
-    
-    $('#candy-13').click(function() {
-        $('#candy-13').hide();
-    })
-    
-    $('#candy-14').click(function() {
-        $('#candy-14').hide();
-    })
-    
-    $('#candy-15').click(function() {
-        $('#candy-15').hide();
-    })
-    
-    $('#candy-16').click(function() {
-        $('#candy-16').hide();
-    })
-    
-    $('#candy-17').click(function() {
-        $('#candy-17').hide();
-    })
-    
-    $('#candy-18').click(function() {
-        $('#candy-18').hide();
-    })
-    
-    $('#candy-19').click(function() {
-        $('#candy-19').hide();
-    })
-    
-    $('#candy-20').click(function() {
-        $('#candy-20').hide();
+$(document).ready(function() {   
+    $('.candy').click(function() {
+        $(this).hide();
     })
 
     $('#reset-btn').click(function() {
-        $('a').show();
+        $('.candy').show();
     })
 })
